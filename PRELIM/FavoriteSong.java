ea619bf6112e5a23e2b1ca5288d77db4c27bde49
karlxxxxz <148021624+karlxxxxz@users.noreply.github.com>
2023-10-25 10:55:32 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+     
+     public static void main(String[] args) {
+      
+          System.out.println("Cause you bring out the best in me ");
+          System.out.println("Like no-one else can do ");
+          System.out.println("That's why I'm by your side ");
+          System.out.println("And that's why I love you ");
+    }
+  }   
+       
